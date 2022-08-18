03ed55c0fb5505b9da64f3fc6d76a0cf7559a4be
Dmitriy <dimadorokh@gmail.com>
2022-08-19 00:04:25 +0300

    bot

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from aiogram.utils import executor
 from create_all import dp
+from models import auth
 
 
 async def on_startup(_):
     print('OK')
 
-from models import auth
 
 auth.register_handlers_auth(dp)
-
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
